d9244a9c28ca064c04f145a33bc3a76314974a01
Maneesha-Lagudu <maneeshalagudu@gmail.com>
2023-10-19 21:15:26 +0530

    Variables

---
+++ Powershell-Sripts/variables.ps1
@@ -0,0 +1,20 @@
+$a = 100  # initializing variable a
+$b = 200 
+# mathematical operations between variables
+$a + $b 
+$a - $b
+$a * $b
+$a % $b
+# Manipulating the values of variable
+$a=300
+$a
+# Storing strings in variables
+$name = 'Maneesha' # strings can be stored either in single-quotes or double-quotes
+$wish = "Hii"
+$wish + $name 
+# We can also reuse the initialized variable in expressions or calculations
+"$a $name"
+"$wish $name" 
+#Other way of using 
+"Hello $name"
+
