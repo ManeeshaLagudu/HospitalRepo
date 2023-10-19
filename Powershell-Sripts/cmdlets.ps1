cf708beedd9ec526b5fd92af29386f99a572935b
Maneesha-Lagudu <maneeshalagudu@gmail.com>
2023-10-19 21:38:13 +0530

    Basic Command-lets

---
+++ Powershell-Sripts/cmdlets.ps1
@@ -0,0 +1,4 @@
+Write-Host "This command-let is used to print the input to console"
+Get-Service # this command shows list of services in the system
+Get-Process # this command show list of process in the system
+Get-Alias # this comand show all the list of aliases defined in the system 
