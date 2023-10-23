f3b774a8d1b9efb30e7d97d26317c77fdbffb7f9
Maneesha-Lagudu <maneeshalagudu@gmail.com>
2023-10-23 10:00:25 +0530

    Hashtable

--- Powershell-Sripts/hashTables.ps1
+++ Powershell-Sripts/hashTables.ps1
@@ -48,28 +48,35 @@ ForEach ($person in $HashTable)
 $HashTable.Count
 <# Here initially we are storing the keys of HashTable in other HashTable named as Extracted_HashTable in that we are storing all the keys
    Now using that new HashTable we can access individual element of it #>
+
 $Extracted_HashTable = $HashTable.Keys
 ForEach ($person in $Extracted_HashTable)
 { "Age of $person is $($HashTable[$person])" } # Here are we are using Grouping operator $() to combine multiple values atonce
+
 # Using Key-Value (pair) Enumeration
 $Enumerated_HashTable = $HashTable.GetEnumerator() # GetEnumerator is method that provides accessibility to the internal elements in a HashTable
 foreach ($pair in $Enumerated_HashTable)
 { "age of $(($pair).Key) is $(($pair).Value)" }
+
 # Making Logics and Conditions using HashTable Keys
 $HashTable.Keys -like "*a*"
 $HashTable."Rahul" -gt 20
 $HashTable.ContainsKey("Monu")
 $HashTable.ContainsValue(30)
+
 # Removing Key-Vaule pairs or contents from the HashTable
 $Blank_HashTable.Remove("Pink")
 $Blank_HashTable
 $Blank_HashTable.Clear() # Using this clear method we can make HashTable empty
 $Blank_HashTable
+
 # Renaming color/ Propoerty names as per our need
 $Updated_HashTable = $HashTable | Format-Table @{name = "User"; expression = { $_.name } }, @{name = "Age"; expression = { $_.value } } # With this we can rename the names of columns to our desired values 
 $Updated_HashTable
+
 # Using Splatting to pass a collection of parameter values to a command as a unit 
 New-item -Path "C:\Users\PS" -Name MyFile.txt -ItemType file -Value "These are my text contents" -Force -ErrorAction Stop # This is the conventional of creating a file
+
 # This file has been created using splatting where all the parameters will be taken same only the name of the file has changed 
 $Creating_File = @{
     Path        = "C:\Users\PS"
@@ -82,9 +89,6 @@ New-Item -Force -Name "File2.txt" @Creating_File
 # Sorting HashTable based on property
 $HashTable.GetEnumerator() | Sort-Object value
 
-# Exporting HashTable to CSV file
-$HashTable | Export-Csv .\exportedHT.csv -NoTypeInformation
-Get-Content .\exportedHT.csv
 # Creating HashTable with keys as column name
 # Using a PSCustomObject is to have a simple way to create structured data
 $Citizen = [PSCustomObject]@{
@@ -94,6 +98,7 @@ $Citizen = [PSCustomObject]@{
     Country = "India"
 }
 $Citizen
+
 # By adding Key-Value pairs as members of HashTable
 $MasterTable = New-Object -TypeName psobject
 $MasterTable | Add-Member -MemberType NoteProperty -Name ComputerName -Value Lenovo
